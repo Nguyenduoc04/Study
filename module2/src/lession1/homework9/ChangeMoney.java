afb270d73c79505933ac2d2901a20d919a767868
Nguyenduoc04 <duocnguyenphuoc159@gmail.com>
2025-12-08 18:45:18 +0700

    Add homework9

---
+++ module2/src/lession1/homework9/ChangeMoney.java
@@ -0,0 +1,11 @@
+package lession1.homework9;
+import java.util.Scanner;
+public class ChangeMoney {
+    public static void main(String[] args) {
+        int money;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter money: ");
+        money = scanner.nextInt();
+        System.out.println("Change: " + money * 23000 + "VND");
+    }
+}
