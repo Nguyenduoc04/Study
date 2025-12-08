f258fbc71b930c347f6e050ab60c275f98bc3fdd
Nguyenduoc04 <duocnguyenphuoc159@gmail.com>
2025-12-08 18:42:39 +0700

    Add homework7

---
+++ module2/src/lession1/homework8/Name.java
@@ -0,0 +1,11 @@
+package lession1.homework8;
+import java.util.Scanner;
+public class Name {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Hello: " + name);
+    }
+}
