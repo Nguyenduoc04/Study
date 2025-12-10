bf3303c31822870fb29749dda9405666f9a4dd08
Nguyenduoc04 <duocnguyenphuoc159@gmail.com>
2025-12-10 11:11:19 +0700

    Add homework5

---
+++ module2/src/lession2/homework5/first20Prime.java
@@ -0,0 +1,28 @@
+package lession2.homework5;
+import java.util.Scanner;
+
+public class first20Prime {
+    public static void main(String[] args) {
+        int numbers;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        numbers = scanner.nextInt();
+        int count = 0;
+        System.out.println("The prime numbers are: ");
+        if(numbers < 2){
+            System.out.println("Invalid number!");
+        }
+        for (int N = 2; N <= numbers; N++) {
+            boolean isPrime = true;
+            for (int i = 2; i <= Math.sqrt(N); i++) {
+                if (N % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                System.out.println(N);
+            }
+        }
+        }
+    }
